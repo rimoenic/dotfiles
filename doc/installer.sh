ab916764dc136db03b80eae3bdea0d16b68fe752
rimoenic <rimoenic@gmail.com>
2021-03-31 14:19:41 +0900

    fix: WSL環境の場合のインストール先調整

--- doc/installer.sh
+++ doc/installer.sh
@@ -2,9 +2,34 @@
 
 DOTPATH=$HOME/.dotfiles
 
-if [[ ! -f  ${DOTPATH}/doc/installer.sh ]]; then
-    mv ${DOTPATH} ${DOTPATH}.orig
-    git clone https://github.com/rimoenic/dotfiles $DOTPATH
+if [ ! -z "$WSLENV" ]; then
+    local TARGET_ENV=wsl
+else
+    local TARGET_ENV=other
+fi
+
+
+
+case $TARGET_ENV in
+    wsl)
+        USERPROFILE_PATH=$(wslpath $(wslvar USERPROFILE))
+        DOTPATH_REAL=${USERPROFILE_PATH}/.dotfiles
+        if [ ! -L ${DOTPATH} ]; then
+            mv ${DOTPATH} ${DOTPATH}.orig || exit 1
+            ln -snfv ${DOTPATH_REAL} ${DOTPATH}
+        fi
+        ;;
+    *)
+        DOTPATH_REAL=${DOTPATH}
+        ;;
+esac
+
+
+
+if [[ ! -f  ${DOTPATH_REAL}/doc/installer.sh ]]; then
+    mv ${DOTPATH_REAL} ${DOTPATH_REAL}.orig || exit 1
+    git clone https://github.com/rimoenic/dotfiles $DOTPATH_REAL
+
 fi
 
 
@@ -12,6 +37,7 @@ fi
 TARGETS=(
     .zshrc
     .dircolors
+    .tmux.conf
 )
 
 for val in ${TARGETS[@]};
