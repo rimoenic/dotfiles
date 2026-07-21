08889f6fcfd36462532056ea44787df88baa2efd
rimoenic <rimoenic@gmail.com>
2026-07-21 12:26:22 +0900

    feat: [home.nix] neovim設定拡張・unfreeパッケージ許可を追加
    programs.neovimにwithRuby/withPython3（旧挙動維持）とextraConfigを追加。
    obsidian等のunfreeパッケージをallowUnfreePredicateで許可。
    flake.lockは通常のinput更新。

--- nix/home.nix
+++ nix/home.nix
@@ -1,4 +1,5 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
+
 
 {
   # ユーザー情報（環境変数から動的に取得）
@@ -10,6 +11,11 @@
 
   # Home Manager自身を有効化
   programs.home-manager.enable = true;
+
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    builtins.elem (lib.getName pkg) [
+      "obsidian"
+    ];
 
   # パッケージ管理
   programs.git = {
@@ -86,7 +92,55 @@
       };
     };
   };
-  programs.neovim.enable = true;
+
+  programs.neovim = {
+    enable = true;
+    withRuby = true;      # 26.05より前の挙動を維持（最新はfalse）
+    withPython3 = true;   # 26.05より前の挙動を維持（最新はfalse）
+
+    extraConfig = ''
+set ignorecase
+set smartcase
+set wrapscan
+set incsearch
+set inccommand=split
+
+set expandtab
+set softtabstop=0
+set tabstop=4
+
+set smarttab
+set shiftwidth=4
+set shiftround
+
+" set statusline=%F%m%r%h%w%=\ [FORMAT=%{&ff}]\ [TYPE=%Y]\ [ASCII=\%03.3b]\
+" [HEX=\%02.2B]\ [POS=%04l,%04v][%p%%]\ [LEN=%L]
+" " ファイル名表示
+" set statusline=%F
+" " 変更チェック表示
+" set statusline+=%m
+" " 読み込み専用かどうか表示
+" set statusline+=%r
+" " ヘルプページなら[HELP]と表示
+" set statusline+=%h
+" " プレビューウインドウなら[Prevew]と表示
+" set statusline+=%w
+" " これ以降は右寄せ表示
+" set statusline+=%=
+" " file encoding
+" set statusline+=[FORMAT=%{&ff}]
+" set statusline+=[ENC=%{&fileencoding}]
+" " 現在行数/全行数
+" set statusline+=[LOW=%l/%L]
+"
+"
+" "
+" ステータスラインを常に表示(0:表示しない、1:2つ以上ウィンドウがある時だけ表示)
+" set laststatus=2
+"
+"
+    '';
+  };
 
   programs.tmux = {
     enable = true;
